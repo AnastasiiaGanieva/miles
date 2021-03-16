8481b89fcdf6159bb68c9e1aaa98ad15852092cb
Anastasiya Ganieva <ganievrav@rambler.ru>
2021-03-16 13:02:11 +0400

    добавление кода

---
+++ Main.java
@@ -0,0 +1,9 @@
+public class Main {
+
+    public static void main (String[] args) {
+int ticket = 8430;
+int miles = ticket/20;
+System.out.println("количество начисленных миль за купленный билет: "+miles);
+    }
+
+}
